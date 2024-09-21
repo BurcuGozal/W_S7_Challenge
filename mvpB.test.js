815f2e9f293882816234e926689320adfb989be6
BurcuGozal <gozalburcu@gmail.com>
2024-09-20 18:32:37 -0600

    done

--- mvpB.test.js
+++ mvpB.test.js
@@ -15,6 +15,28 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [5] sum('10', '3') // returns 13
   */
 
+    test("should throw an error if there is no passed arguments", () => {
+      expect(() => sum()).toThrow("pass valid numbers")
+    })
+
+    test("should throw an error if there is non-numeric string passed", () => {
+      expect(() => sum(2, "seven")).toThrow("pass valid numbers")
+    })
+
+    test("should return 4 when sum(1, 3) is called", () => {
+      expect(sum(1, 3)).toBe(4)
+    })
+
+    test('should return 3 when sum("1", 2) is called', () => {
+      expect(sum("1", 2)).toBe(3)
+    })
+
+    test('should return 13 when sum("10", "3") is called', () => {
+      expect(sum("10", "3")).toBe(13)
+    }) 
+
+
+
   /*
   👉 TASK 2 - Integration Testing of HelloWorld component at the bottom of this module
 
@@ -29,9 +51,45 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [5] renders a text that reads "JavaScript is pretty awesome"
     [6] renders a text that includes "javaScript is pretty" (use exact = false)
   */
-  test('you can comment out this test', () => {
-    expect(true).toBe(false)
+  // test('you can comment out this test', () => {
+  //   expect(true).toBe(false)
+  // })
+beforeEach(() => {
+  render(<HelloWorld />)
+})
+  test('renders a link that reads "Home"', () => {
+    const homeLink = screen.queryByText("Home")
+    expect(homeLink).toBeInTheDocument()
   })
+
+
+  test('renders a link that reads "About"', () => {
+    const aboutLink = screen.queryByText("About");
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  test('renders a link that reads "Blog' , () => {
+    const blogLink = screen.queryByText("Blog")
+    expect (blogLink).toBeInTheDocument()
+  })
+
+  test('renders a link that reads "The Truth"', () => {
+    const truthText = screen.queryByText("The Truth")
+    expect(truthText).toBeInTheDocument()
+  })
+
+  test('renders a link that reads "JavaScript is pretty awesome"', () => {
+    const javaScriptText = screen.queryByText("JavaScript is pretty awesome")
+    expect(javaScriptText).toBeInTheDocument()
+  })
+
+  test('renders a link that reads "javaScript is pretty"', () => {
+    const partialText = screen.queryByText(/javascript is pretty/i, {exact: false})
+    expect(partialText).toBeInTheDocument()
+  });
+
+
+
 })
 
 function sum(a, b) {
